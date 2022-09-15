6880b900161de7f5e08520f416fed461667f95c8
joao.gois <joao.gois@olist.com>
2022-09-15 15:26:59 -0300

    Finish studies up 2022-09-15-15h:26m

---
+++ 21_channels/1-entendendo-channels/4-buffer-block.go
@@ -0,0 +1,35 @@
+/*
+- Canais são o Jeito Certo® de fazer sincronização e código concorrente.
+- Eles nos permitem trasmitir valores entre goroutines.
+- Servem pra coordenar, sincronizar, orquestrar, e buffering.
+- Na prática:
+    - make(chan type, b)
+- Canais bloqueiam:
+    - Eles são como corredores em uma corrida de revezamento
+    - Eles tem que "passar o bastão" de maneira sincronizada
+    - Se um corredor tentar passar o bastão pro próximo, mas o próximo corredor não estiver lá...
+    - Ou se um corredor ficar esperando receber o bastão, mas ninguem entregar...
+    - ...não dá certo.
+- Exemplos:
+    - Poe um valor num canal e faz um print. Block.
+        - Código acima com goroutine.
+        - Ou com buffer. Via de regra: má idéia; é legal em certas situações, mas em geral é melhor sempre passar o bastão de maneira sincronizada.
+- Interessante: ref/spec → types
+- Código:
+    - Block: https://play.golang.org/p/dClS7vQlYE (não roda!)
+    - Go routine: https://play.golang.org/p/ZbNCwUuiPi
+    - Buffer: https://play.golang.org/p/32vYvCR7qn
+    - Buffer block: https://play.golang.org/p/smeW6vigAT
+    - Mais buffer: https://play.golang.org/p/Pe2pcboGiA
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	canal := make(chan int, 1)
+	canal <- 42
+    canal <- 43
+	fmt.Println(<-canal)
+}
---
+++ 21_channels/2-canais-unidirecionais-e-utilizando-canais/4-atribuicao-de-tipos.go
@@ -0,0 +1,56 @@
+/*
+Canais direcionais
+
+- Canais podem ser direcionais.
+- E isso serve pra...?
+- Um send channel e um receive channel são tipos diferentes. Isso permite que os type-checking mechanisms do compilador façam com que não seja possível, por exemplo, escrever num canal de leitura.
+- Aos aventureitos: https://stackoverflow.com/questions/1...
+- Canais bidirecionals (send & receive)
+    - send chan←
+        - error: "invalid operation: ←cs (receive from send-only type chan← int)"
+    - receive ←chan
+        - error: "invalid operation: cr ← 42 (send to receive-only type ←chan int)"
+- Exemplo: https://play.golang.org/p/TlcSm8bHkW
+- A seta sempre aponta para a esquerda.
+- Assignment/conversion:
+    - de geral para específico
+    - de específico para geral não
+    - Exemplos:
+        - geral pra específico: https://play.golang.org/p/H1uk4YGMBB
+        - específico pra específico: https://play.golang.org/p/8JkOnEi7-a
+        - específico pra geral: https://play.golang.org/p/4sOKuQRHq7
+        - atribuição tipos !=: https://play.golang.org/p/bG7H6l03VQ
+
+Utilizando canais
+
+- Em funcs podemos especificar:
+    - receive channel
+        - Parâmetro receive channel: (c ←chan int)
+        - No scope dessa função, esse canal só recebe
+        - Não podemos fechar um receive channel
+    - send channel
+        - Parâmetro send channel: (c chan← int)
+        - No scope dessa função, esse canal só envia
+        - Podemos fechar um send channel
+- Exemplo: passando informação de uma função para outra.
+- Código: https://play.golang.org/p/TlcSm8bHkW (replay)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+	cr := make(<-chan int) // receive
+	cs := make(chan<- int) // send
+
+	fmt.Println("------")
+	fmt.Printf("c\t%T\n", c)
+	fmt.Printf("cr\t%T\n", cr)
+	fmt.Printf("cs\t%T\n", cs)
+
+	// Specific to general doesn't assign
+	c = cr
+    c = cs
+}
